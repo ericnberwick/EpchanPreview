00778fc58a5971609be66ede55f671324ffb5994
ericnberwick <nb15galle@gmail.com>
2024-07-11 17:41:20 +0100

    Update 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,9 +7,16 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public : {
       CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_KEY: process.env.CONTENTFUL_ACCESS_KEY
+    CONTENTFUL_ACCESS_KEY: process.env.CONTENTFUL_ACCESS_KEY,
+     CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT || 'master'
 
     }
   }
+
+  console.log('Contentful Configurations:');
+  console.log('Space ID:', process.env.CONTENTFUL_SPACE_ID);
+  console.log('Access Token:', process.env.CONTENTFUL_ACCESS_TOKEN);
+  console.log('Environment:', process.env.CONTENTFUL_ENVIRONMENT);
+
 
 })
