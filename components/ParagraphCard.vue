d4ca3725469fc1b6bd31af4f074d84e4f2d07e8e
ericnberwick <nb15galle@gmail.com>
2024-10-08 18:20:35 +0100

    Prototyle fml

--- components/ParagraphCard.vue
+++ components/ParagraphCard.vue
@@ -1,25 +1,31 @@
 <template>
-    <ul class="mt-4  ">
-        <li class="">
-            <a class="block h-full rounded-lg border border-gray-700 p-4 transition-all ">
-                <strong v-if="paragraph.fields.title != 'notitle'" class="font-medium text-white text-xl">{{
-                    paragraph.fields.title }}</strong>
+    <div :class="`top-[${top}]`" class="sticky -mt-48 mb-48 rounded-t-[46px] border-2 bg-gray-800  px-5 py-10 sm:px-20">
+        <div class="mb-14 flex gap-8 text-2xl font-bold text-white">
 
+            <p class="underline" v-if="paragraph.fields.title != 'notitle'">{{ paragraph.fields.order + " " +
+                paragraph.fields.title }}
+            </p>
+            <p class="underline" v-if="paragraph.fields.title == 'notitle'">{{ paragraph.fields.order }}
+                Introduction
+            </p>
+        </div>
+        <div class="flex flex-col-reverse gap-8 sm:gap-20 lg:flex-row lg:items-center">
+            <div class="max-w-2xl">
+                <img src="public/img/building.jpg" alt="" />
+            </div>
+            <div class="max-w-7xl  ">
 
-                <p class="mt-1 text-lg font-medium text-gray-300 ">
+                <p class="text-2xl text-[#636262]   text-gray-100">
                     <RichTextRenderer :document="paragraph.fields.body" />
                 </p>
-            </a>
-        </li>
-
-
-
-    </ul>
+            </div>
+        </div>
+    </div>
 
 </template>
 
 <script setup>
-import { defineProps, onMounted } from 'vue'
+import { defineProps, onMounted, ref } from 'vue'
 import RichTextRenderer from 'contentful-rich-text-vue-renderer';
 
 const props = defineProps({
@@ -29,10 +35,16 @@ const props = defineProps({
     }
 })
 
-//const praiseExists = ref(false);
+
+const topInt = ref(((parseInt(props.paragraph.fields.order) - 1) * 6))
+const top = ref(topInt.value.toString() + "rem");
+
 
 onMounted(() => {
-    console.log("yoo : ", props.paragraph.fields)
+    if (topInt.value > 30) {
+        topInt.value = 30;
+        top.value = "30rem"
+    }
 
 });
 </script>
