0c0b74d02e56f9663494d0ca02786a347745f5f7
ericnberwick <nb15galle@gmail.com>
2024-11-05 18:21:48 +0000

    Workshop reviews CMS managed

--- pages/workshops.vue
+++ pages/workshops.vue
@@ -29,7 +29,33 @@
       </div>
       <BonusCourseCard class="mt-10 "></BonusCourseCard>
       <div class="mt-10">
-        <Review></Review>
+        <section class="bg-gray-800">
+          <div class="mx-auto max-w-screen-2xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+            <div class="md:flex md:items-end md:justify-between">
+              <div class="">
+                <h2 class="text-4xl font-bold tracking-tight text-white sm:text-5xl">
+                  Reviews from our customers
+                </h2>
+              </div>
+            </div>
+
+            <div class="mt-8 grid grid-cols-1 gap-4 md:grid-cols-3">
+              <WorkshopReviewCard v-for="review in reviews" :key="review.sys.id" :review="review"></WorkshopReviewCard>
+              <blockquote class="flex h-full flex-col justify-center items-center bg-white p-6 shadow-sm sm:p-8">
+                <div>
+                  <div class="mt-4">
+                    <p class="text-4xl font-bold text-blue-600 ">Enroll Now</p>
+                    <a href=" https://buy.stripe.com/fZe4ig3Gf1rsfN64gj">
+                      <button
+                        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg border border-green-500 text-green-500 hover:opacity-75 focus:ring focus:ring-green-200 active:opacity-[0.85] block w-full mt-6"
+                        type="button">Enroll now</button>
+                    </a>
+                  </div>
+                </div>
+              </blockquote>
+            </div>
+          </div>
+        </section>
       </div>
       <Footer class="mt-20"></Footer>
     </section>
@@ -41,11 +67,13 @@ import { ref, onMounted } from "vue";
 import { useRuntimeConfig } from '#imports';
 import BonusCourseCard from "../components/BonusCourseCard.vue";
 import LoadingCard from "../components/LoadingCard.vue";
+import WorkshopReviewCard from "../components/WorkshopReviewCard.vue";
 
 const config = useRuntimeConfig();
 const courses = ref([]);
 const spaceName = config.public.CONTENTFUL_SPACE_ID;
 const accessTokenName = config.public.CONTENTFUL_ACCESS_KEY;
+const reviews = ref([]);
 
 let client;
 const isLoading = ref(true);
@@ -61,6 +89,12 @@ async function fetchEntries() {
     content_type: "courseCard",
   });
   courses.value = res.items;
+
+  const reviewRes = await client.getEntries({
+    content_type: "workshopReviews",
+  });
+
+  reviews.value = reviewRes.items;
 }
 
 onMounted(() => {
