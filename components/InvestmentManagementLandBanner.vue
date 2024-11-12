b723ae02a6d7e17ce9bb12f0c25e1475655577aa
ericnberwick <nb15galle@gmail.com>
2024-11-12 16:56:06 +0000

    QTS removed transitions

--- components/InvestmentManagementLandBanner.vue
+++ components/InvestmentManagementLandBanner.vue
@@ -1,31 +1,27 @@
 <template>
   <div class="grid grid-cols-5 gap-4 ">
     <div class="col-span-3 flex items-center justify-center">
-      <ScrollTransition>
-        <div class="ml-48 m-4  w-min lg:ml-0">
-          <h2 class=" font-bold text-7xl w-min">QTS</h2>
-          <h2 class=" font-bold text-7xl w-min">Capital</h2>
-          <h2 class=" font-bold text-7xl w-min">Management</h2>
-          <h3 class="mt-4  text-xl font-medium text-gray-300  ">{{ bannerData.fields.description }}</h3>
-          <div class="mt-4 ">
-            <a class="inline-flex items-center gap-4 rounded border border-indigo-600 bg-indigo-600 px-16 py-4 text-white hover:bg-indigo-800 transition-all hover:text-gray-100 focus:outline-none focus:ring active:text-indigo-500"
-              href="https://qtscm.com/" target="_blank">
-              <span class="text-sm font-medium"> Find out more </span>
+      <div class="ml-48 m-4  w-min lg:ml-0">
+        <h2 class=" font-bold text-7xl w-min">QTS</h2>
+        <h2 class=" font-bold text-7xl w-min">Capital</h2>
+        <h2 class=" font-bold text-7xl w-min">Management</h2>
+        <h3 class="mt-4  text-xl font-medium text-gray-300  ">{{ bannerData.fields.description }}</h3>
+        <div class="mt-4 ">
+          <a class="inline-flex items-center gap-4 rounded border border-indigo-600 bg-indigo-600 px-16 py-4 text-white hover:bg-indigo-800 transition-all hover:text-gray-100 focus:outline-none focus:ring active:text-indigo-500"
+            href="https://qtscm.com/" target="_blank">
+            <span class="text-sm font-medium"> Find out more </span>
 
-              <svg class="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-              </svg>
-            </a>
-          </div>
+            <svg class="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+            </svg>
+          </a>
         </div>
-      </ScrollTransition>
+      </div>
     </div>
 
     <div class="col-span-2 col-start-4 lg:block hidden">
-      <ScrollTransition>
-        <img :src="'https:' + bannerData.fields.picture.fields.file.url" width="600">
-      </ScrollTransition>
+      <img :src="'https:' + bannerData.fields.picture.fields.file.url" width="600">
     </div>
   </div>
 </template>
--- pages/investment-management.vue
+++ pages/investment-management.vue
@@ -1,89 +1,89 @@
 <template>
   <div class="bg-gray-800  h-screen">
     <Navbar page="investment-management" />
-    <Transition name="fade">
-      <div v-if="!isLoading" class="px-8 py-24 bg-gray-800 text-white">
-        <div class="container mx-auto text-center">
-          <h2 class="block antialiased tracking-normal font-sans text-4xl font-semibold leading-[1.3] text-white mb-4">
-            Investment Management</h2>
-          <p class="block antialiased font-sans text-base leading-relaxed text-inherit mb-8 font-normal !text-gray-500">
-          </p>
-        </div>
 
-        <div class="w-full h-56 overflow-hidden mb-8">
-          <NuxtImg src="img/finances.jpg" class="w-full h-full object-cover object-center rounded-xl"
-            style="object-position: center top;"></NuxtImg>
-        </div>
+    <div v-if="!isLoading" class="px-8 py-24 bg-gray-800 text-white">
+      <div class="container mx-auto text-center">
+        <h2 class="block antialiased tracking-normal font-sans text-4xl font-semibold leading-[1.3] text-white mb-4">
+          Investment Management</h2>
+        <p class="block antialiased font-sans text-base leading-relaxed text-inherit mb-8 font-normal !text-gray-500">
+        </p>
+      </div>
 
-        <div class="">
-          <InvestmentManagementLandBanner :bannerData="d" v-for="d in bannerData" :key="d.sys.id">
-          </InvestmentManagementLandBanner>
+      <div class="w-full h-56 overflow-hidden mb-8">
+        <NuxtImg src="img/finances.jpg" class="w-full h-full object-cover object-center rounded-xl"
+          style="object-position: center top;"></NuxtImg>
+      </div>
 
-          <ScrollTransition>
-            <div class="mt-16 grid grid-cols-1 lg:grid-cols-3">
-              <div>
-                <div class="flex items-center">
-                  <img src="public/img/calender.svg" width="16px" class="m-4 ">
-                  <h2 class="text-4xl font-bold">Founded in 2011</h2>
-                </div>
-                <p class="m-4 text-lg font-medium text-gray-300 ">Founded in 2011 by quant hedge fund manager and quant
-                  finance author Dr. Ernest Chan, QTS Capital Managment LLC is a global multi-strategy investment
-                  management
-                  firm.</p>
+      <div class="">
+        <InvestmentManagementLandBanner :bannerData="d" v-for="d in bannerData" :key="d.sys.id">
+        </InvestmentManagementLandBanner>
+
+        <ScrollTransition>
+          <div class="mt-16 grid grid-cols-1 lg:grid-cols-3">
+            <div>
+              <div class="flex items-center">
+                <img src="public/img/calender.svg" width="16px" class="m-4 ">
+                <h2 class="text-4xl font-bold">Founded in 2011</h2>
               </div>
-              <div>
-                <div class="flex items-center">
-                  <img src="public/img/chart-line.svg" width="16px" class="m-4 ">
-                  <h2 class="text-4xl font-bold">Data Driven</h2>
-                </div>
-                <p class="m-4 text-lg font-medium text-gray-300 ">Led by a team of mathematicians and physicists QTS has
-                  developed a proprietary quantitative approach to
-                  strategy development and selection with a strong emphasis on uncorrelated risk-adjusted returns and
-                  risk
-                  mitigation. </p>
-              </div>
-              <div>
-                <div class="flex items-center">
-                  <img src="public/img/fa-book.svg" width="16px" class="m-4 ">
-                  <h2 class="text-4xl font-bold">Programs</h2>
-                </div>
-
-                <p class="m-4 text-lg font-medium text-gray-300 ">QTS Capital Management LLC manages a Commodity Pool as
-                  well as individual clients’ accounts using quantitative strategies. It is a member of the National
-                  Futures
-                  Association and is registered as a Commodity Pool Operator and Commodity Trading Advisor (NFA ID:
-                  0431162). Since 2011, it has served a global clientele spanning 4 continents. </p>
-              </div>
+              <p class="m-4 text-lg font-medium text-gray-300 ">Founded in 2011 by quant hedge fund manager and quant
+                finance author Dr. Ernest Chan, QTS Capital Managment LLC is a global multi-strategy investment
+                management
+                firm.</p>
             </div>
-          </ScrollTransition>
-
-          <ScrollTransition>
-            <div class="mt-16 grid grid-cols-1">
-              <div class="flex justify-center">
-                <h2 class="text-4xl font-bold tracking-tight text-white sm:text-5xl">
-                  Investor Reviews
-                </h2>
+            <div>
+              <div class="flex items-center">
+                <img src="public/img/chart-line.svg" width="16px" class="m-4 ">
+                <h2 class="text-4xl font-bold">Data Driven</h2>
               </div>
-              <div>
-                <section class="bg-gray-800">
-                  <div class="  px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-                    <div class="md:flex md:items-end md:justify-between">
-                      <div class="max-w-xl">
-                      </div>
-                    </div>
+              <p class="m-4 text-lg font-medium text-gray-300 ">Led by a team of mathematicians and physicists QTS has
+                developed a proprietary quantitative approach to
+                strategy development and selection with a strong emphasis on uncorrelated risk-adjusted returns and
+                risk
+                mitigation. </p>
+            </div>
+            <div>
+              <div class="flex items-center">
+                <img src="public/img/fa-book.svg" width="16px" class="m-4 ">
+                <h2 class="text-4xl font-bold">Programs</h2>
+              </div>
 
-                    <div class="mt-8 grid gap-4 grid-cols-1 lg:grid-cols-2">
-                      <InvestorManagementReview v-for="review in reviews" :key="review.sys.id" :review="review">
-                      </InvestorManagementReview>
+              <p class="m-4 text-lg font-medium text-gray-300 ">QTS Capital Management LLC manages a Commodity Pool as
+                well as individual clients’ accounts using quantitative strategies. It is a member of the National
+                Futures
+                Association and is registered as a Commodity Pool Operator and Commodity Trading Advisor (NFA ID:
+                0431162). Since 2011, it has served a global clientele spanning 4 continents. </p>
+            </div>
+          </div>
+        </ScrollTransition>
+
+        <ScrollTransition>
+          <div class="mt-16 grid grid-cols-1">
+            <div class="flex justify-center">
+              <h2 class="text-4xl font-bold tracking-tight text-white sm:text-5xl">
+                Investor Reviews
+              </h2>
+            </div>
+            <div>
+              <section class="bg-gray-800">
+                <div class="  px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+                  <div class="md:flex md:items-end md:justify-between">
+                    <div class="max-w-xl">
                     </div>
                   </div>
-                </section>
-              </div>
+
+                  <div class="mt-8 grid gap-4 grid-cols-1 lg:grid-cols-2">
+                    <InvestorManagementReview v-for="review in reviews" :key="review.sys.id" :review="review">
+                    </InvestorManagementReview>
+                  </div>
+                </div>
+              </section>
             </div>
-          </ScrollTransition>
-        </div>
+          </div>
+        </ScrollTransition>
       </div>
-    </Transition>
+    </div>
+
     <div>
       <Footer></Footer>
     </div>
